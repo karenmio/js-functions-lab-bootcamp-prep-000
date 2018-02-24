ca7f66148c41429fa1c5140db248787d4b8a5aea
karen wang <karenkarenmio@gmail.com>
2018-02-24 18:13:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,9 +7,7 @@ function happyHolidaysTo(name){
   return(`Happy holidays, ${name}!`);
 }
 
-function happyHolidaysTo(holiday,name){
-  return(`Happy ${holiday}, ${name}!`);
-}
+
 
 function holidayCountdown(holiday,days){
   return(`It's ${days} days until ${holiday}!`);
